46d1a35b7951787cfc2d5e919b3a81627c6e2afa
Alan-Vazquez <vazalan17@ciencias.unam.mx>
2024-09-17 19:41:35 -0600

    Add files via upload

--- Practica02.hs
+++ Practica02.hs
@@ -1,33 +1,48 @@
----Listas y recursion---
+--------------- Listas y recursión ---------------
 
-----1. Longitud de una lista----
+--Longitud de una lista--
 longitud :: [a] -> Int
 longitud [] = 0
-longitud (x:xs) = 1 + longitud(xs)
+longitud (x:xs) = 1 + longitud (xs) 
 
-----2. Suma de n numeros---
+--Suma de n numeros de una lista--
 sumaLista :: Num a => [a] -> a
 sumaLista [] = 0
 sumaLista (x:xs) = x + sumaLista xs
 
-----3. Agregar elemento a una lista----
+--Agregar un elemento a una lista--
 agregaElemento :: [a] -> a -> Bool -> [a]
-agregaElemento lista elem True  = elem : lista   -- Inserta al principio
-agregaElemento lista elem False = lista ++ [elem] -- Inserta al final
+agregaElemento [] a booleano = a : []
+agregaElemento (x:xs) a booleano = if booleano 
+                    then a : (x:xs)
+                    else (x:xs) ++ [a]
 
-----4. Maximo de una lista----
+--Maximo de una lista--
 maximoLista :: (Num a, Ord a) => [a] -> a
 maximoLista [] = error "La lista no puede estar vacia"
 maximoLista [x] = x
 maximoLista (x:xs) = max x (maximoLista xs)
 
-----5. Recuperar un elemento de una lista de acuerdo a su índice----
-.
-.
-.
+--Recuperar un elemento segun un indice--
+indice :: [a] -> Int -> a
+indice [] y = error "La lista no tiene elementos"
+indice (x:xs) y = if y < 0 || y >= longitud (x:xs)
+                    then error "El indice esta fuera del rango"
+                    else if y == 0
+                            then x
+                            else indice xs (y-1)
 
----Listas por comprehensi ́on y recursi ́on de cola.---
+--------------- Listas por comprehensión ---------------
 
-----1. Divisores de un n ́umero entero.----
+divisores :: Int -> [Int]
+divisores _ = undefined
 
-----2. Convertir una lista en conjunto----
+conjunto :: Eq a => [a] -> [a]
+conjunto _ = undefined
+
+--Obtener los numeros de pares de una lista--
+numerosPares :: [Int] -> [Int]
+numerosPares [] = []
+numerosPares (x:xs) = if x `mod` 2 == 0
+                        then x : numerosPares xs
+                        else numerosPares xs
